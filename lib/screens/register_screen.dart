52242c98d190f865fce62ccfe4a72c9dfdfeac26
bgebes <berkay.gebes@bil.omu.edu.tr>
2022-12-30 02:44:16 +0300

    feat: Register Screen kodlandi

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:hellodoc/helpers/variable_breakpoints.dart';
+import 'package:hellodoc/widgets/buttoned_text.dart';
 import 'package:hellodoc/widgets/custom_button.dart';
 import 'package:hellodoc/widgets/logo.dart';
 import 'package:hellodoc/widgets/textbox.dart';
 
-class ResetScreen extends StatefulWidget {
-  const ResetScreen({super.key});
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
 
   @override
-  State<ResetScreen> createState() => _ResetScreen();
+  State<RegisterScreen> createState() => _RegisterScreen();
 }
 
-class _ResetScreen extends State<ResetScreen> {
+class _RegisterScreen extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     GlobalKey<FormState>? formKey = GlobalKey<FormState>();
-    
+
     final emailController = TextEditingController();
+    final passwordController = TextEditingController();
+    final passwordRController = TextEditingController();
 
     var logoSection = SizedBox(
       height: size.height / 2,
@@ -26,30 +29,43 @@ class _ResetScreen extends State<ResetScreen> {
       ),
     );
 
-    var textStyle = const TextStyle(
-      color: Colors.white,
-      fontSize: 24,
-      fontWeight: FontWeight.bold,
+    var bottomTextSection = Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        ButtonedText(
+          onTap: () {},
+          innerText: "Zaten bir hesabınız var mı?",
+        ),
+      ],
     );
 
     var form = Form(
       key: formKey,
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text(
-            "Sıfırlama bağlantısı gönder",
-            style: textStyle,
-          ),
-          const SizedBox(height: 64),
           Textbox(
             controller: emailController,
             validator: (v) => null,
             hintText: "E-posta adresiniz",
           ),
-            const SizedBox(height: 16),
-          CustomButton(onTap: () {}, innerText: "Sıfırla")
+          const SizedBox(height: 16),
+          Textbox(
+            controller: passwordController,
+            validator: (v) => null,
+            hintText: "Parolanız",
+          ),
+          const SizedBox(height: 16),
+          Textbox(
+            controller: passwordRController,
+            validator: (v) => null,
+            hintText: "Parolanız",
+          ),
+          const SizedBox(height: 16),
+          CustomButton(onTap: () {}, innerText: "İlerle"),
+          const SizedBox(height: 16),
+          bottomTextSection,
         ],
       ),
     );

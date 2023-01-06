56139142a2c181d61f1f7d6fb304ed3f638d48f3
bgebes <berkay.gebes@bil.omu.edu.tr>
2023-01-06 20:18:58 +0300

    feat: HelpScreen ve HelpDetailsScreen olusturuldu

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hellodoc/helpers/variable_breakpoints.dart';
-import 'package:hellodoc/screens/doctor_profile_screen.dart';
-import 'package:hellodoc/screens/patient_screen_seen_bydoctor.dart';
+import 'package:hellodoc/screens/chat_screen.dart';
+import 'package:hellodoc/screens/doctors_profile_seen_bypatient.dart';
+import 'package:hellodoc/screens/help_details_screen.dart';
+import 'package:hellodoc/screens/help_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +21,7 @@ class MyApp extends StatelessWidget {
         primaryColor: colors["primary"],
         appBarTheme: AppBarTheme(color: colors["primary"], elevation: 0),
       ),
-      home: const DoctorProfileScreen(),
+      home: const HelpDetailsScreen(),
       debugShowCheckedModeBanner: false,
     );
   }

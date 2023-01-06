ab30e605894771c1713ce0017b73cdd8d22c3392
bgebes <berkay.gebes@bil.omu.edu.tr>
2023-01-06 19:28:31 +0300

    feat: MessageBox, ChatScreen olusturulup textbox optimize edildi

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hellodoc/helpers/variable_breakpoints.dart';
-import 'package:hellodoc/screens/patient_screen_seen_bydoctor.dart';
+import 'package:hellodoc/screens/chat_screen.dart';
+import 'package:hellodoc/screens/doctors_profile_seen_bypatient.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         primaryColor: colors["primary"],
         appBarTheme: AppBarTheme(color: colors["primary"], elevation: 0),
       ),
-      home: const PatientScreenSeenByDoctor(),
+      home: const ChatScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
